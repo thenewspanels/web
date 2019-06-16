36b53f644741ab32a8b5a62d48328132cc8dee59
Travis CI User <travis@example.org>
2019-06-16 02:58:09 +0000

    Auto-commit: make deploy

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('//service-worker.js?v=1560609972').then(function(reg) {
+            navigator.serviceWorker.register('//service-worker.js?v=1560653889').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
